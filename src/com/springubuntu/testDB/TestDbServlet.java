c2bd42fc89f5f1e2dabb916d228680952a8c761b
saiashish9 <saiashish3760@gmail.com>
2020-12-21 00:23:42 +0530

    initial commit

--- src/com/springubuntu/testDB/TestDbServlet.java
+++ src/com/springubuntu/testDB/TestDbServlet.java
@@ -1,6 +1,8 @@
 package com.springubuntu.testDB;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,7 +17,29 @@ public class TestDbServlet extends HttpServlet {
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		String user = "springstudent";
+		String pass = "springstudent";
+		
+		String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false";
+		String driver = "com.mysql.cj.jdbc.Driver";
+		
+		try {
+			
+			PrintWriter out = response.getWriter();
+			out.println(jdbcUrl);
+			Class.forName(driver);
+			Connection myConn = DriverManager.getConnection(jdbcUrl,user,pass);
+			myConn.close();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			throw new ServletException(e);
+		}
+		
+		
+		
+	
 	}
 
 }
