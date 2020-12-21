1481669daa14733bd5464e96f98fb974ec5a2c71
saiashish9 <saiashish3760@gmail.com>
2020-12-21 21:24:41 +0530

    dao entity repository controller updated

--- WebContent/WEB-INF/view/list-customers.jsp
+++ WebContent/WEB-INF/view/list-customers.jsp
@@ -1,10 +1,33 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
 </head>
-<body>List Customers
+<body>
+
+	<div id="container">
+
+		<div id="content">
+			<table>
+				<tr>
+					<th>First Name</th>
+					<th>Last Name</th>
+					<th>Email</th>
+				</tr>
+				<c:forEach var="temp" items="${customers}">
+					<tr>
+						<td>${temp.firstName}</td>
+						<td>${temp.lastName}</td>
+						<td>${temp.email}</td>
+					</tr>
+				</c:forEach>
+			</table>
+		</div>
+
+	</div>
 
 </body>
 </html>
---
+++ src/com/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,33 @@
+package com.springdemo.dao;
+
+import java.util.List;
+
+import org.hibernate.query.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.springdemo.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+	
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		
+		Session current= sessionFactory.getCurrentSession();
+		
+		Query<Customer> theQuery = current.createQuery("from Customer",Customer.class);
+		
+		List<Customer> customers = theQuery.getResultList();
+		
+		return customers;
+	}
+
+}
